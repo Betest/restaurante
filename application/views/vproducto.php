2b52d9c04c33192172cb870016e35defaeacad6d
camilofox <camilo.ensamble@gmail.com>
2019-11-05 20:33:57 -0500

    hola

--- application/views/vproducto.php
+++ application/views/vproducto.php
@@ -323,7 +323,7 @@
             
         </div>
         <div class="form-group">
-            <h3>Valor5745</h3>
+            <h3>Valor</h3>
             
                 <input type="text" id="txtvalue" name="txtvalue" class="form-control" style="width:40%;" value="<?= (isset($detailproducto[0])) ? $detailproducto[0]->value : ""; ?>">
            
